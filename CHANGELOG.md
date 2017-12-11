7f4639d2acbf70aa497b618fdf2a0ea6ca2a0863
Russ Miles <russ@russmiles.com>
2017-12-11 18:47:59 +0000

    First version of extension template
    Contributes to #1

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Changelog
+
+## [Unreleased][]
+
+[Unreleased]: https://github.com/chaostoolkit/chaostoolkit-extension-template/compare/0.1.0...HEAD
+
+### Added
+
+-   Initial release
---
+++ chaosext/probes.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+import json
+import os.path
+from typing import Union
+
+from chaoslib.exceptions import FailedActivity
+from chaoslib.types import MicroservicesStatus, Secrets
+import yaml
